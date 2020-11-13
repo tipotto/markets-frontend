519cf1bbec2e937c6645ede9bf7194951fb276a2
tipotto <label1134@gmail.com>
2020-11-13 11:24:11 +0900

    reducerのコードの微調整

--- src/reducers/domain/search.js
+++ src/reducers/domain/search.js
@@ -1,13 +1,16 @@
 import initState from "../initState";
 import { SUCCEEDED_SEARCH, LOAD_ITEMS, LOAD_ITEM_NUMBER } from "../../actions";
 
-const searchReducer = (state = initState.search, action) => {
-  switch (action.type) {
+const searchReducer = (
+  state = initState.search,
+  { type, resData, moldedData } = {}
+) => {
+  switch (type) {
     case SUCCEEDED_SEARCH:
       return {
         ...state,
-        items: action.resData.items,
-        loaded: action.resData.loaded,
+        items: resData.items,
+        loaded: resData.loaded,
         loadItemNum: state.loadItemNum + LOAD_ITEM_NUMBER,
       };
 
@@ -15,8 +18,9 @@ const searchReducer = (state = initState.search, action) => {
       // 元の配列に、新しく取得した配列をマージする。
       return {
         ...state,
-        items: action.moldedData.items,
-        loaded: state.loaded.concat(action.moldedData.loaded),
+        items: moldedData.items,
+        loaded: state.loaded.concat(moldedData.loaded),
+        // loaded: [ ...state.loaded, ...moldedData.loaded],
         loadItemNum: state.loadItemNum + LOAD_ITEM_NUMBER,
       };
 
@@ -24,5 +28,30 @@ const searchReducer = (state = initState.search, action) => {
       return state;
   }
 };
+
+// const searchReducer = (state = initState.search, action) => {
+//   switch (action.type) {
+//     case SUCCEEDED_SEARCH:
+//       return {
+//         ...state,
+//         items: action.resData.items,
+//         loaded: action.resData.loaded,
+//         loadItemNum: state.loadItemNum + LOAD_ITEM_NUMBER,
+//       };
+
+//     case LOAD_ITEMS:
+//       // 元の配列に、新しく取得した配列をマージする。
+//       return {
+//         ...state,
+//         items: action.moldedData.items,
+//         loaded: state.loaded.concat(action.moldedData.loaded),
+//         // loaded: [ ...state.loaded, ...action.moldedData.loaded],
+//         loadItemNum: state.loadItemNum + LOAD_ITEM_NUMBER,
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
 
 export default searchReducer;
