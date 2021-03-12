b7a5c4fc016e0993dbcfdfee0db360057a25b11c
tipotto <label1134@gmail.com>
2021-03-12 13:57:38 +0900

    update to display platform logos on each item

--- src/components/organisms/Item.jsx
+++ src/components/organisms/Item.jsx
@@ -1,34 +1,36 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import mercariLogo from "../../images/mercari.png";
+import rakumaLogo from "../../images/rakuma.png";
+import paypayLogo from "../../images/paypay.png";
 
 const useStyles = makeStyles(() => ({
   root: {
-    width: '300px',
-    marginTop: '35px',
-    marginLeft: '35px',
-    display: 'inline-block',
+    width: "300px",
+    marginTop: "35px",
+    marginLeft: "35px",
+    display: "inline-block",
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%", // 16:9
   },
-  avatar: {
-    backgroundColor: red[500],
-    fontSize: '5px',
-  },
+  // avatar: {
+  //   backgroundColor: red[500],
+  //   fontSize: "5px",
+  // },
   content: {
     paddingBottom: 0,
   },
   typo: {
-    fontSize: '17px',
-    textAlign: 'center',
+    fontSize: "17px",
+    textAlign: "center",
   },
 }));
 
@@ -38,26 +40,32 @@ const Item = (props) => {
 
   const moldTitle = () => {
     const MAX_LENGTH = 30;
-    if (!title) return '';
+    if (!title) return "";
     if (title.length <= MAX_LENGTH) return title;
     return `${title.substr(0, MAX_LENGTH)}...`;
   };
 
   const moldPrice = () => {
-    return String(price).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+    return String(price).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  };
+
+  const setIcon = (platform) => {
+    let logo;
+    if (platform === "mercari") {
+      logo = mercariLogo;
+    } else if (platform === "rakuma") {
+      logo = rakumaLogo;
+    } else {
+      logo = paypayLogo;
+    }
+
+    return <Avatar alt="service logo" src={logo} />;
   };
 
   return (
     <a href={detail} target="_blank" rel="noopener noreferrer">
       <Card className={classes.root}>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="recipe" className={classes.avatar}>
-              {platform}
-            </Avatar>
-          }
-          title={moldTitle()}
-        />
+        <CardHeader avatar={setIcon(platform)} title={moldTitle()} />
         <CardMedia
           className={classes.media}
           image={image}
